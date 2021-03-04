f73cd36c3a60e184ab245dafca6413513911982a
Tim Newton <manicmaniac_2000@yahoo.com>
2021-03-04 18:32:02 +0000

    Modified to handle multiple word strings

--- substrings.rb
+++ substrings.rb
@@ -1,15 +1,18 @@
+# Find entries in the dictionary that also exist in the string, remove those that don't exist
 def find_entries(str, dictionary)
-    mapped_dictionary = dictionary.select { |entry| entry if str.downcase.include? entry.downcase }
-    mapped_dictionary
+    dictionary.select { |entry| entry if str.downcase.include? entry.downcase }
+end
+
+# Count the number of occurences of a particular string in another string
+def count_occurences(string_to_inspect, string_to_find)
+    string_to_inspect.downcase.scan(/#{string_to_find}/).count
 end
 
 def substrings(str, dictionary)
-    # Select only the elements from the dictionary that appear in the reference string
-    selected_entries = find_entries(str, dictionary)
 
     # Tally the number of occurences of an entry and put the result in a hash
-    selected_entries.reduce(Hash.new(0)) do |word_tally, entry|
-        word_tally[entry] += 1
+    find_entries(str, dictionary).reduce(Hash.new(0)) do |word_tally, entry|
+        word_tally[entry] += count_occurences(str, entry)
         word_tally
     end
 end
